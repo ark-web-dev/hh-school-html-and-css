947eda39fd87fb1cfdfb9112a7f0efa02808b3e8
Leonid Feskov <leonik89@gmail.com>
2023-11-21 19:34:12 +0300

    Updates

---
+++ components.f92e137a.js
@@ -0,0 +1,3 @@
+const e=document.createElement("div");e.className="grid",document.body.appendChild(e),document.addEventListener("keydown",d=>{d.ctrlKey&&"KeyG"===d.code&&e.classList.toggle("grid_visible")});//# sourceMappingURL=components.f92e137a.js.map
+
+//# sourceMappingURL=components.f92e137a.js.map
