f7393c731a958093c703dbf7966ba9cdc4a6387a
Arkadiy <webdev-projects@yandex.ru>
2023-12-02 22:35:49 +0300

    Updates

---
+++ index.f66a3d94.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded",function(){new Slider(document.querySelector(".slider"),8e3)});//# sourceMappingURL=index.f66a3d94.js.map
+
+//# sourceMappingURL=index.f66a3d94.js.map
