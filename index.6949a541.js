86aea4612354736a85a6c4d6c8d5d6eb597f0124
Arkadiy <webdev-projects@yandex.ru>
2023-12-02 22:19:12 +0300

    Updates

--- index.6949a541.js
+++
@@ -1,3 +0,0 @@
-const e=document.createElement("div");e.className="grid",document.body.appendChild(e),document.addEventListener("keydown",d=>{d.ctrlKey&&"KeyG"===d.code&&e.classList.toggle("grid_visible")});//# sourceMappingURL=index.6949a541.js.map
-
-//# sourceMappingURL=index.6949a541.js.map
