6c60ad99c695239e3e341d01c7f25412885aa435
Arkadiy <webdev-projects@yandex.ru>
2023-12-02 22:31:06 +0300

    Updates

---
+++ index.d0a7e23a.js
@@ -0,0 +1,6 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    new Slider(document.querySelector(".slider"), 8000);
+});
+
+//# sourceMappingURL=index.d0a7e23a.js.map
